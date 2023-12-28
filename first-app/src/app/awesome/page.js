f94875908bd685724c5b9a8ec32b993fa34ba057
Marxu <fuhaojun@52tt.com>
2023-12-28 20:41:54 +0800

    feat: 添加一些照片 初始化 food 项目

--- first-app/src/app/awesome/page.js
+++
@@ -1,8 +0,0 @@
-export default function AwesomePage(){
-    console.log('object'); // 会在 服务端 打印 
-    return (
-        <main>
-            <h1>NextJS Is Awesome</h1>
-        </main>
-    )
-}
--- first-app/src/app/page.js
+++ first-app/src/app/page.js
@@ -1,13 +1,9 @@
-import Link from 'next/link'
-import Header from '@/components/header.js'
-
 export default function Home() {
   return (
     <main>
-     <Header/>
-     <p>
-        <Link href='/about'>About us</Link>
-     </p>
+      <h1 style={{ color: 'white', textAlign: 'center' }}>
+        Time to get started!
+      </h1>
     </main>
-  )
+  );
 }
