6b34ed0be3c135cd88642a5349f237349ec6da8a
Marxu <fuhaojun@52tt.com>
2024-01-03 15:28:45 +0800

    feat: 图片展示

--- first-app/src/app/page.js
+++ first-app/src/app/page.js
@@ -2,13 +2,16 @@ import Link from 'next/link';
 
 import classes from './page.module.css';
 import MainHeader from '@/components/main-header/main-header';
+import ImageSlideshow from '@/components/images/image-slideshow';
 
 export default function Home() {
   return (
     <>
     <MainHeader/>
       <header className={classes.header}>
-        <div className={classes.slideshow}></div>
+        <div className={classes.slideshow}>
+            <ImageSlideshow/>
+        </div>
         <div>
           <div className={classes.hero}>
             <h1>NextLevel Food for NextLevel Foodies</h1>
