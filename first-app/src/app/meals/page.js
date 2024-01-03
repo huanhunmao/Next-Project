002d25705cbe0e6549bcd35b49734587fb9a8424
Marxu <fuhaojun@52tt.com>
2024-01-03 18:37:29 +0800

    feat: 使用 db 中的数据

--- first-app/src/app/meals/page.js
+++ first-app/src/app/meals/page.js
@@ -1,7 +1,11 @@
 import MealsGrid from '@/components/meals/meal-grid'
 import classes from './page.module.css'
 import Link from 'next/link'
-export default function MealPage(){
+import { getMeals } from '@/lib/getMeals'
+
+export default async function MealPage(){
+    const meals = await getMeals()
+
     return (
         <>
         <header className={classes.header}>
@@ -16,7 +20,7 @@ export default function MealPage(){
             </p>
         </header>
         <main className={classes.main}>
-            <MealsGrid meals={[]}/>
+            <MealsGrid meals={meals}/>
         </main>
         </>
     )
