95130a99d3f7fc2cbe0ee35136bbe13a0c2bcbff
Marxu <fuhaojun@52tt.com>
2024-01-03 17:58:48 +0800

    feat: 添加 meals page 内容

--- first-app/src/app/meals/page.js
+++ first-app/src/app/meals/page.js
@@ -1,3 +1,23 @@
+import MealsGrid from '@/components/meals/meal-grid'
+import classes from './page.module.css'
+import Link from 'next/link'
 export default function MealPage(){
-    return <h1>Meal page</h1>
+    return (
+        <>
+        <header className={classes.header}>
+            <h1>
+                Delicious meals, created {''}
+                <span className={classes.highlight}>by you</span>
+            </h1>
+            <p className={classes.cta}>
+                <Link href='/meals/share'>
+                    Share Your Favorites Recipe
+                </Link>
+            </p>
+        </header>
+        <main className={classes.main}>
+            <MealsGrid meals={[]}/>
+        </main>
+        </>
+    )
 }
