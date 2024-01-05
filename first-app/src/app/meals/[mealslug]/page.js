d8dbbd26bd64d74295afb57f454ff765545de76c
Marxu <fuhaojun@52tt.com>
2024-01-05 15:55:15 +0800

    feat: 显示图片并展示 动态路由部分

--- first-app/src/app/meals/[mealslug]/page.js
+++ first-app/src/app/meals/[mealslug]/page.js
@@ -1,4 +1,35 @@
 // if u visit this url--> http://localhost:3000/meals/xxx
-export default function MealDetailPage(){
-    return <h1>meal detail page</h1>
+import { getMeal } from '@/lib/getMeals'
+import classes from './page.module.css'
+import Image from 'next/image'
+
+export default  function MealDetailPage({params}){
+    const meal =  getMeal(params.mealslug)
+
+    meal.instructions = meal.instructions.split(/\n/g).join('<br/>');
+    console.log('meal.instructions',meal.instructions);
+
+    return (
+        <>
+        <header className={classes.header}>
+            <div className={classes.image}>
+                <Image src={meal.image} alt={meal.title} fill/>
+            </div>
+            <div className={classes.headerText}>
+                <h1>{meal.title}</h1>
+                <p className={classes.creator}>
+                    by <a href={`mailto:${meal.creator_email}`}>NAME</a>
+                </p>
+                <p className={classes.summary}>SUMMARY</p>
+            </div>
+        </header>
+        <main>
+            <p className={classes.instructions}
+            dangerouslySetInnerHTML={{
+                __html:meal.instructions
+            }}>
+            </p>
+        </main>
+        </>
+    )
 }
