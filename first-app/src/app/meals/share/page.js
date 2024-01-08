af39485d88da54e1b685c92b1f2156ac75bf705e
Marxu <fuhaojun@52tt.com>
2024-01-08 14:50:48 +0800

    feat: 添加图片选择器

--- first-app/src/app/meals/share/page.js
+++ first-app/src/app/meals/share/page.js
@@ -1,3 +1,4 @@
+import ImagePicker from '@/components/meals/image-picker';
 import classes from './page.module.css';
 
 export default function ShareMealPage() {
@@ -38,7 +39,7 @@ export default function ShareMealPage() {
               required
             ></textarea>
           </p>
-          IMAGE PICKER
+          <ImagePicker name='Choose file' label='Image Picker'/>
           <p className={classes.actions}>
             <button type="submit">Share Meal</button>
           </p>
