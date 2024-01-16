49b54afb279034b8d826d45486870f574737b34a
Marxu <fuhaojun@52tt.com>
2024-01-16 15:51:25 +0800

    feat: 路由动态匹配

--- page-router-nextjs-starting-project/pages/clients/[id]/index.js
+++ page-router-nextjs-starting-project/pages/clients/[id]/index.js
@@ -4,9 +4,27 @@ function ClientProjectPage(){
     const router = useRouter()
 
     console.log(router.query); // {id: '123'}
+
+    function loadProjectHandler(){
+        // load data
+
+        // 第一种写法 
+        // router.push('/clients/max/projectsMax') // {id: 'max', clientprojectid: 'projectsMax'}
+
+        // 第二种写法 
+        router.push({
+            pathname: '/clients/[id]/[clientprojectid]',
+            query: {
+                id: 'max',
+                clientprojectid: 'projectsMax'
+            }
+        })
+        }
+    
     return (
         <div>
             <h1>The client project Page</h1>
+            <button onClick={loadProjectHandler}>Load Page A</button>
         </div>
     )
 }
