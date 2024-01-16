820986594b7d02d33ab32305b1d69dc61813ee20
Marxu <fuhaojun@52tt.com>
2024-01-16 18:20:11 +0800

    feat: item-list 页面基本 骨架

--- nextjs-03-prj-routing-starting-setup/components/events/event-item.js
+++ nextjs-03-prj-routing-starting-setup/components/events/event-item.js
@@ -1,10 +1,38 @@
+import Link  from 'next/link'
+
 function EventItem(props){
-    const {items} = props;
+    const {title, image, date, location, id} = props;
+    console.log('title',title);
+
+    const humanReadableDate = new Date(date).toLocaleDateString('en-US',{
+        year: 'numeric',
+        month: 'long',
+        day: 'numeric',
+      })
+
+    const formattedAddress = location && location.replace(', ', '\n')
+    const exploreLink = `/events/${id}`
 
     return (
-        <ul>
-            {items.map(item => <li></li>)}
-        </ul>
+        <li>
+            <img src={'/' + image} alt=''/>
+            <div>
+                <div>
+                    <div>
+                    <h2>{title}</h2>
+                    <div>
+                        <time>{humanReadableDate}</time>
+                    </div>
+                    <div>
+                        <address>{formattedAddress}</address>
+                    </div>
+                    </div>
+                    <div>
+                    <Link href={exploreLink}>Explore Event</Link>
+                    </div>
+                </div>
+            </div>
+        </li>
     )
 }
 
--- nextjs-03-prj-routing-starting-setup/components/events/event-list.js
+++ nextjs-03-prj-routing-starting-setup/components/events/event-list.js
@@ -1,11 +1,18 @@
 import EventItem from './event-item'
 
 function EventList(props){
-    const {items} = props;
+    const { items } = props;
 
     return (
         <ul>
-            {items.map(item => <EventItem/>)}
+            {items.map(event => <EventItem 
+            key={event.id}
+            id={event.id}
+            title={event.title}
+            locations={event.locations}
+            date={event.date}
+            image={event.image}
+            />)}
         </ul>
     )
 }
