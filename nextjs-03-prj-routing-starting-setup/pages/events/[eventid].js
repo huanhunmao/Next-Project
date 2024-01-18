596808a5137a05915f0a48c8419fb943229e6e35
Marxu <fuhaojun@52tt.com>
2024-01-18 18:03:34 +0800

    feat: 添加 event detail page(支持 比如 http://localhost:3000/events/e3 )

--- nextjs-03-prj-routing-starting-setup/pages/events/[eventid].js
+++ nextjs-03-prj-routing-starting-setup/pages/events/[eventid].js
@@ -1,8 +1,35 @@
+import {useRouter} from 'next/router'
+
+import EventSummary from "../../components/event-detail/event-summary"
+import EventContent from "../../components/event-detail/event-content"
+import EventLogistics from "../../components/event-detail/event-logistics"
+
+import {getEventById} from '../../dummy-data'
 
 function EventDetailPage(){
+    const router = useRouter()
+
+    const eventId = router.query.eventid
+    const event = getEventById(eventId)
+
+    if(!event){
+        return <p>No event found!</p>
+    }
+
+    const { title, date, location, image, description } = event
+
     return (
         <div>
-            <h1>The Event Detail Page </h1>
+           <EventSummary title={title}/>
+           <EventLogistics
+            date={date}
+            address={location}
+            image={image}
+            imageAlt={title}
+            />
+           <EventContent>
+                <p>{description}</p>
+           </EventContent>
         </div>
     )
 }
