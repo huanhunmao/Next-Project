5a512ebab7a5e582774a3dd3934bb57d3c2ab366
Marxu <fuhaojun@52tt.com>
2024-01-29 17:25:20 +0800

    feat: 添加 Event search ui 部分

--- nextjs-03-prj-routing-starting-setup/pages/events/index.js
+++ nextjs-03-prj-routing-starting-setup/pages/events/index.js
@@ -1,10 +1,12 @@
 import EventList from '../../components/events/event-list'
+import EventSearch from '../../components/events/events-search'
 import {getAllEvents} from '../../dummy-data'
 
 function EventPage(){
     const events = getAllEvents()
     return (
         <div>
+            <EventSearch />
             <EventList items={events}/>
         </div>
     )
