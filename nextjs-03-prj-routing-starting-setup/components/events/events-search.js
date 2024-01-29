3cf57c5bf9efbb6a958cb31d6467f6c1c74b6f89
Marxu <fuhaojun@52tt.com>
2024-01-29 18:19:11 +0800

    feat: 添加 events search 逻辑部分

--- nextjs-03-prj-routing-starting-setup/components/events/events-search.js
+++ nextjs-03-prj-routing-starting-setup/components/events/events-search.js
@@ -1,20 +1,34 @@
+import { useRef } from 'react'
 import Button from "../../ui/button"
 import classes from '../../styles/events-search.module.css'
 
-function EventSearch(){
+function EventSearch(props){
+    const yearInput = useRef()
+    const monthInput = useRef()
+
+    function submitHandler(event){
+        event.preventDefault()
+
+        const selectedYear = yearInput.current.value 
+        const selectedMonth = monthInput.current.value
+
+        props.onSearch(selectedYear, selectedMonth)
+    }
+
+
     return (
-        <form className={classes.form}>
+        <form className={classes.form} onSubmit={submitHandler}>
             <div className={classes.controls}>
                 <div className={classes.control}>
                     <label htmlFor="year">Year</label>
-                    <select id='year'>
+                    <select id='year' ref={yearInput}>
                         <option>2021</option>
                         <option>2022</option>
                     </select>
                 </div>
                 <div className={classes.control}>
                     <label htmlFor="month">Month</label>
-                    <select id='month'>
+                    <select id='month' ref={monthInput}>
                     <option value='1'>January</option>
                     <option value='2'>February</option>
                     <option value='3'>March</option>
