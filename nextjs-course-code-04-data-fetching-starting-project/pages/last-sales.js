4e2cafaeb722e4cc6616a88189fa06f53a369126
Marxu <fuhaojun@52tt.com>
2024-03-18 20:19:31 +0800

    feat: useSWR 获取数据 官方文档有坑 小心

--- nextjs-course-code-04-data-fetching-starting-project/pages/last-sales.js
+++ nextjs-course-code-04-data-fetching-starting-project/pages/last-sales.js
@@ -1,42 +1,74 @@
 import {useState, useEffect} from 'react'
+import useSWR from 'swr';
 
 function LastSalesPage(){
-    const [data, setData] = useState()
-    const [isLoading, setIsLoading] = useState(false)
-    useEffect(() => {
-        setIsLoading(true)
-        fetch('https://nextjs-50eda-default-rtdb.firebaseio.com/sales.json',)
-            .then(data => data.json())
-            .then(data => {
-                const transformData = []
+    const [sales, setSales] = useState()
 
-                for(let key in data){
-                    transformData.push({
-                        id: key,
-                        username: data[key].username,
-                        volume: data[key].volume
-                    })
-                }
-                console.log(transformData)
-                setData(transformData)
+    // 定义一个异步获取数据的函数
+const fetchData = async () => {
+    const response = await fetch('https://nextjs-50eda-default-rtdb.firebaseio.com/sales.json');
+    const data = await response.json();
+        const transformData = []
+        for(let key in data){
+            transformData.push({
+                id: key,
+                username: data[key].username,
+                volume: data[key].volume
+            })
+        }
 
-                setIsLoading(false)})
-            .catch(e => console.error(e))
-    },[])
+        setSales(transformData)
+        return data;
+  };
 
+  
+    const { data, error } = useSWR('data',fetchData);
+    console.log(data);
 
-    if(isLoading){
-        return <p>Loading ...</p>
+    if (error) {
+        return <p>Has errors</p>;
     }
 
-    if(!data){
-        return <p>No data yet</p>
+    if (!data || !sales) {
+        return <p>Loading ...</p>;
     }
+
+    const transformData = Object.entries(data).map(([id, { username, volume }]) => ({
+        id,
+        username,
+        volume,
+    }));
+
+    if (transformData.length === 0) {
+        return <p>No data yet</p>;
+    }
+
+    // useEffect(() => {
+    //     setIsLoading(true)
+    //     fetch('https://nextjs-50eda-default-rtdb.firebaseio.com/sales.json',)
+    //         .then(data => data.json())
+    //         .then(data => {
+    //             const transformData = []
+    //
+    //             for(let key in data){
+    //                 transformData.push({
+    //                     id: key,
+    //                     username: data[key].username,
+    //                     volume: data[key].volume
+    //                 })
+    //             }
+    //             console.log(transformData)
+    //             setSales(transformData)
+    //
+    //             setIsLoading(false)})
+    //         .catch(e => console.error(e))
+    // },[])
+
 
     return (
         <ul>
         {
-            data.map(item => (
+            sales.map(item => (
                 <li key={item.id}>
                     {item.username}-${item.volume}
                 </li>
