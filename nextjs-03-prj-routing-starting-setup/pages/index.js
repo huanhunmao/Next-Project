a498663e9968d14b68458b564a859778131bf387
Marxu <fuhaojun@52tt.com>
2024-03-19 11:26:18 +0800

    feat: 使用 swr 获取数据库 firebase 数据 而不是虚拟数据

--- nextjs-03-prj-routing-starting-setup/pages/index.js
+++ nextjs-03-prj-routing-starting-setup/pages/index.js
@@ -1,8 +1,38 @@
+import { useState } from "react";
 import EventList from "../components/events/event-list"
-import { getFeaturedEvents } from "../dummy-data"
+// import { getFeaturedEvents } from "../dummy-data"
+import useSWR  from 'swr'
 
 function HomePage(){
-    const featuredEvents = getFeaturedEvents()
+    const [featuredEvents, setFeaturedEvents] = useState([])
+    // const featuredEvents = getFeaturedEvents()
+
+    const fetchData = async () => {
+        const response = await fetch('https://nextjs-50eda-default-rtdb.firebaseio.com/sales.json');
+        const data = await response.json();
+            const transformData = []
+            for(let key in data){
+                transformData.push({
+                    id: key,
+                    isFeatured: data[key].isFeatured
+                })
+            }
+    
+            const transformDatas = Object.values(data.events).filter(e => e.isFeatured)
+            setFeaturedEvents(transformDatas)
+            return data;
+      };
+    
+      
+        const { data, error } = useSWR('data',fetchData);
+
+        if(!data){
+            return <p>Loading ...</p>
+        }
+
+        if(error){
+            return <p>Has errors</p>
+        }
 
     return (
         <div>
