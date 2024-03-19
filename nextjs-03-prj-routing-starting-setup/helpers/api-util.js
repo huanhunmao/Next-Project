3a5e841931af62b1873801e2a2f7a41f6e5c1bde
Marxu <fuhaojun@52tt.com>
2024-03-19 17:43:36 +0800

    feat: 这样写 讲道理 应该能行 但是 还是不行

--- nextjs-03-prj-routing-starting-setup/helpers/api-util.js
+++ nextjs-03-prj-routing-starting-setup/helpers/api-util.js
@@ -1,21 +1,27 @@
 
-// export async function getAllEvents() {
-//     const response = await fetch('https://nextjs-50eda-default-rtdb.firebaseio.com/events.json');
+export async function getAllEvents() {
+    const response = await fetch('https://nextjs-50eda-default-rtdb.firebaseio.com/events.json');
 
-//     const data = await response.json();
-//     const events = []
-//     for (const key of data){
-//         events.push({
-//             id:key,
-//             ...data[key],
-//         })
-//     }
+    const data = await response.json();
+    console.log('data',data);
+    // const events = []
+    // for (const key of data){
+    //     events.push({
+    //         id:key,
+    //         ...data[key],
+    //     })
+    // }
 
-//     return events;
-// }
+    // return events;
+}
 
 // export async function getFeaturedEvents(){
 //     const allEvents = await getAllEvents()
 
 //     return Object.values(allEvents).filter(e => e.isFeatured)
 // }
+
+export async function getEventById(id){
+    const allEvents = await getAllEvents()
+    return allEvents.find((event) => event.id === id);
+}
--- nextjs-03-prj-routing-starting-setup/pages/events/[eventid].js
+++ nextjs-03-prj-routing-starting-setup/pages/events/[eventid].js
@@ -1,22 +1,23 @@
-import {useRouter} from 'next/router'
+// import {useRouter} from 'next/router'
 
 import EventSummary from "../../components/event-detail/event-summary"
 import EventContent from "../../components/event-detail/event-content"
 import EventLogistics from "../../components/event-detail/event-logistics"
 
-import {getEventById} from '../../dummy-data'
+// import {getEventById} from '../../dummy-data'
+import { getEventById, getAllEvents } from '../../helpers/api-util'
 
-function EventDetailPage(){
-    const router = useRouter()
+function EventDetailPage(props){
+    // const router = useRouter()
 
-    const eventId = router.query.eventid
-    const event = getEventById(eventId)
+    // const eventId = router.query.eventid
+    // const event = getEventById(eventId)
 
-    if(!event){
+    if(!props.event){
         return <p>No event found!</p>
     }
 
-    const { title, date, location, image, description } = event
+    const { title, date, location, image, description } = props.event
 
     return (
         <div>
@@ -32,6 +33,29 @@ function EventDetailPage(){
            </EventContent>
         </div>
     )
+}
+
+export  async function getStaticProps(context){
+    const eventid = context.query.eventid 
+
+    const event = await getEventById(eventid)
+    console.log('event',event);
+    return {
+        props: {
+            event
+        }
+    }
+}
+
+export async function getStaticPaths(){
+    const events = await getAllEvents()
+
+    const paths = events.map(event => ({params:{ eventid: event.id}}))
+
+    return {
+        paths: paths,
+        fallback: false
+    }
 }
 
 export default EventDetailPage
