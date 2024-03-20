edf04158ce3ecfa911d5e90c0a6f87cf6526e4e4
Marxu <fuhaojun@52tt.com>
2024-03-20 14:33:08 +0800

    feat: 如果存在多个 Head nextjs 会进行合并操作 一般会在 _app.js  设一个 Head 避免其他页面忘记设置 而且其他页面设置了  会替换掉这个

--- nextjs-03-prj-routing-starting-setup/pages/_app.js
+++ nextjs-03-prj-routing-starting-setup/pages/_app.js
@@ -6,6 +6,7 @@ function MyApp({ Component, pageProps }) {
   return (
     <Layout>
         <Head>
+            <title>Next Events</title>
             <meta name='viewport' content='initial-scale=1.0, width=device-width'/>
         </Head>
         <Component {...pageProps} />
