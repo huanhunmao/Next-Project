6d1ca0a38c47be7cb3094d3db9fe8f07e4d912e0
Marxu <fuhaojun@52tt.com>
2024-03-26 14:08:44 +0800

    feat: 模拟给后端 post 传递数据

--- nextjs-course-code-07-api-routes-starting-project/pages/api/feedback.js
+++ nextjs-course-code-07-api-routes-starting-project/pages/api/feedback.js
@@ -1,8 +1,33 @@
+import fs from 'fs';
+import path from 'path';
 
 function handler(req, res){
-    res.status(200).json({
-        message: 'It works!'
-    })
+    if(req.method === 'POST'){
+        const email = req.body.email;
+        const feedbackText = req.body.text 
+
+        const newFeedBack = {
+            id: new Date().toISOString(),
+            email: email,
+            text: feedbackText
+        }
+
+        // store that in database or in a file 
+        const filePath = path.join(process.cwd(), 'data', 'feedback.json')
+        const fileData = fs.readFileSync(filePath)
+        const data = JSON.parse(fileData)
+        data.push(newFeedBack)
+        fs.writeFileSync(filePath, JSON.stringify(data), 'utf8')
+        res.status(201).json({
+            message: 'Success!',
+            feedback: newFeedBack
+        })
+
+    }else{
+        res.status(200).json({
+            message: 'It works!'
+        })
+    }
 }
 
 export default handler
--- nextjs-course-code-07-api-routes-starting-project/pages/index.js
+++ nextjs-course-code-07-api-routes-starting-project/pages/index.js
@@ -1,7 +1,42 @@
+import { useRef } from "react";
+
 function HomePage() {
+    const emailInputRef = useRef()
+    const feedbackInputRef = useRef()
+
+    function onSubmitFeedback(event){
+        event.preventDefault()
+
+        const email = emailInputRef.current.value
+        const text = feedbackInputRef.current.value
+        const data = {
+            email,
+            text
+        }
+
+        fetch('/api/feedback', {
+            method: 'POST',
+            headers: {
+              'Content-Type': 'application/json', 
+            },
+            body: JSON.stringify(data),
+          })
+            .then(response => response.json())
+            .then(data => console.log(data))
+            .catch(error => console.error('Error:', error));
+
+    }
+
   return (
     <div>
       <h1>The Home Page</h1>
+      <form onSubmit={onSubmitFeedback}>
+        <label>Enter your Email</label>
+        <input type='email' id='email'  ref={emailInputRef}/>
+        <label>Enter your feedback</label>
+        <textarea id='feedback' rows="5" ref={feedbackInputRef}/>
+        <button type="submit">Send Your Feedback</button>
+        </form>
     </div>
   );
 }
