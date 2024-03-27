6758962714fa04605b7060d16af1f0c524d95ba9
Marxu <fuhaojun@52tt.com>
2024-03-27 20:32:34 +0800

    feat: add Context State

--- nextjs-course-code-08-prj-api-routes-starting-project/store/notification-context.js
+++ nextjs-course-code-08-prj-api-routes-starting-project/store/notification-context.js
@@ -7,8 +7,24 @@ const NotificationContext = createContext({
 });
 
 export function NotificationContextProvider(props){
+    const [activeNotification, setActiveNotification] = useState()
+
+    function showNotificationHandler(notificationData){
+        setActiveNotification(notificationData)
+    }
+
+    function hideNotificationHandler(){
+        setActiveNotification(null)
+    }
+
+    const context = {
+        notification: activeNotification,
+        showNotification: showNotificationHandler,
+        hideNotification: hideNotificationHandler
+    }
+
     return (
-        <NotificationContext.Provider>
+        <NotificationContext.Provider value={context}>
             {props.children}
         </NotificationContext.Provider>
     )
