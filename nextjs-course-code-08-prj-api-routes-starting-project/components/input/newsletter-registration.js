81935b3a39601b094eb480a25fe54b4e7f82eb25
Marxu <fuhaojun@52tt.com>
2024-03-28 15:42:37 +0800

    feat: 将 全局通知放在 注册时 ， 注意⚠️  _app.js 外层的 Provider 需要包裹上  组件内才能消费 context

--- nextjs-course-code-08-prj-api-routes-starting-project/components/input/newsletter-registration.js
+++ nextjs-course-code-08-prj-api-routes-starting-project/components/input/newsletter-registration.js
@@ -1,25 +1,53 @@
-import { useRef } from 'react';
+import { useRef, useContext } from 'react';
+
 import classes from './newsletter-registration.module.css';
+import NotificationContext from '../../store/notification-context';
 
 function NewsletterRegistration() {
-    const emailInputRef = useRef()
+  const emailInputRef = useRef();
+  const notificationCtx = useContext(NotificationContext);
+
   function registrationHandler(event) {
-    const data = {email:emailInputRef.current.value}
     event.preventDefault();
 
-    // fetch user input (state or refs)
-    // optional: validate input
-    // send valid data to API
+    const enteredEmail = emailInputRef.current.value;
+
+    notificationCtx.showNotification({
+      title: 'Signing up...',
+      message: 'Registering for newsletter.',
+      status: 'pending',
+    });
+
     fetch('/api/newsletter', {
-        method: 'POST',
-        headers: {
-          'Content-Type': 'application/json', 
-        },
-        body: JSON.stringify(data),
+      method: 'POST',
+      body: JSON.stringify({ email: enteredEmail }),
+      headers: {
+        'Content-Type': 'application/json',
+      },
+    })
+      .then((response) => {
+        if (response.ok) {
+          return response.json();
+        }
+
+        return response.json().then((data) => {
+          throw new Error(data.message || 'Something went wrong!');
+        });
       })
-        .then(response => response.json())
-        .then(data => console.log(data))
-        .catch(error => console.error('Error:', error));
+      .then((data) => {
+        notificationCtx.showNotification({
+          title: 'Success!',
+          message: 'Successfully registered for newsletter!',
+          status: 'success',
+        });
+      })
+      .catch((error) => {
+        notificationCtx.showNotification({
+          title: 'Error!',
+          message: error.message || 'Something went wrong!',
+          status: 'error',
+        });
+      });
   }
 
   return (
--- nextjs-course-code-08-prj-api-routes-starting-project/pages/_app.js
+++ nextjs-course-code-08-prj-api-routes-starting-project/pages/_app.js
@@ -1,20 +1,24 @@
 import Head from 'next/head';
 
 import Layout from '../components/layout/layout';
+import { NotificationContextProvider } from '../store/notification-context';
 import '../styles/globals.css';
-import Notification from '../components/ui/notification/notification';
 
 function MyApp({ Component, pageProps }) {
   return (
-    <Layout>
-      <Head>
-        <title>Next Events</title>
-        <meta name='description' content='NextJS Events' />
-        <meta name='viewport' content='initial-scale=1.0, width=device-width' />
-      </Head>
-      <Component {...pageProps} />
-      <Notification status='pending'/>
-    </Layout>
+    <NotificationContextProvider>
+      <Layout>
+        <Head>
+          <title>Next Events</title>
+          <meta name='description' content='NextJS Events' />
+          <meta
+            name='viewport'
+            content='initial-scale=1.0, width=device-width'
+          />
+        </Head>
+        <Component {...pageProps} />
+      </Layout>
+    </NotificationContextProvider>
   );
 }
 
